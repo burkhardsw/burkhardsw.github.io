1b18c2376139174655243ae1658f94b56573c7b2
andreglock <andreglock@gmail.com>
2025-06-09 14:58:12 +0200

    make mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="topWrapper">
+        <div className="textWrapper">
+          <h1>Ich bin Burkhard</h1>
+          <h2>Das ist meine Geschichte</h2>
+        </div>
+        <div className="imagePlaceholder">Image Placeholder</div>
       </div>
-      <h1>Vite + React</h1>
-      <h2>Title 2</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="mainText">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+          consequatur blanditiis est incidunt alias? Error nesciunt iure enim?
+          Iste sed vitae laboriosam! Quas unde eum delectus dignissimos animi.
+          Minus, nobis. Ipsam cupiditate velit modi quas natus! Culpa itaque
+          laudantium assumenda, sed expedita rem ex quo eveniet. Illo et labore
+          dolores sint molestiae rem ipsum, laborum assumenda vitae fugit,
+          architecto fugiat. Accusantium, possimus animi! Culpa corporis fuga
+          est quibusdam magnam dignissimos accusantium dolorum architecto fugit
+          ex dolore quo, perferendis recusandae ratione aliquam corrupti
+          molestiae quia? Exercitationem voluptatibus id quam voluptates ipsa.
+          Ipsa excepturi voluptatem, tempora et facilis sit doloribus quia
+          numquam necessitatibus ratione inventore quis neque eos nam earum
+          veritatis, eveniet sunt tempore aspernatur natus debitis pariatur.
+          Quam beatae necessitatibus aspernatur. Ea tenetur ut voluptatum
+          delectus reprehenderit deserunt, debitis sint, tempora labore
+          doloremque odio unde officia dicta, eligendi quibusdam velit impedit
+          numquam ducimus dolores consectetur vel. Quisquam delectus doloribus
+          rerum nobis? Asperiores rerum deleniti sunt aliquid reiciendis
+          consequuntur ullam ad eius repellat! Ipsum molestias perferendis unde
+          tenetur temporibus culpa totam pariatur dignissimos! Tempore et
+          voluptatum esse similique, sed impedit quod iure. Ex quaerat quasi
+          pariatur a, obcaecati soluta minus molestias cupiditate alias ab hic
+          id ipsa accusamus mollitia porro similique placeat numquam fugiat
+          inventore ad velit quo asperiores adipisci distinctio. Veritatis?
+          Obcaecati, nisi soluta deleniti molestiae sequi exercitationem ullam
+          dolorem inventore animi doloribus optio accusantium maxime dignissimos
+          quae dolor eum dolore quasi quo id! Rem, nulla ipsam. A laudantium
+          commodi labore? Exercitationem, explicabo. Fugiat ipsam officiis
+          distinctio. Beatae vero, voluptates eaque dolore excepturi aliquam.
+          Doloribus nisi velit debitis repellat magni explicabo obcaecati totam
+          officiis voluptate unde, rerum, cumque nam alias similique! Ducimus
+          dolorum voluptatibus deserunt adipisci dolor sed veniam! Consectetur
+          dolorem molestias eum. Doloremque eaque porro nesciunt aut neque?
+          Dicta, suscipit? Nemo incidunt quas numquam corporis neque molestiae
+          velit distinctio quo!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
